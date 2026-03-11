2e90d1d21c893181ed2f0d8d19f66c37d6830ad1
Saowapan K <my@saowapan.com>
2026-03-11 16:24:32 +0700

    Git Action PR and Jest Test

---
+++ server/tests/learnJest.test.js
@@ -0,0 +1,34 @@
+const LearnJest = require('../learnJest');
+
+describe('learnJest', () => {
+  let learnJest;
+
+  beforeEach(() => {
+    learnJest = new LearnJest();
+  });
+
+  test('Should add two positive number correctly', () => {
+    // Arrange
+    const a = 5;
+    const b = 3;
+    const expected = 8;
+
+    // Act
+    const result = learnJest.add(a, b);
+
+    // Assert
+    expect(result).toBe(expected);
+  });
+
+  test('arrays', () => {
+    const arr1 = [1, 2, 3];
+    const arr2 = [1, 2, 3];
+    expect(arr1).toEqual(arr2);
+  });
+
+  test('objects', () => {
+    const obj1 = { name: 'May', age: 18 };
+    const obj2 = { name: 'May', age: 18 };
+    expect(obj1).toEqual(obj2);
+  })
+});
